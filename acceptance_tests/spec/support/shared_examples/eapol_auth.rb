5ad8d0bf50e39274b09909c67c74741b0f768ad3
Anthony King <anthony@madetech.com>
2019-02-05 09:48:59 +0000

    utilise spec_helper for tests, and introduce shared examples to enforce consistent behaviour checks

---
+++ acceptance_tests/spec/support/shared_examples/eapol_auth.rb
@@ -0,0 +1,73 @@
+
+shared_examples 'a success logger' do
+  # needs :db
+  # needs :eapol_test_command
+
+  before do
+    eapol_test_command
+  end
+
+  it 'logs the authentication' do
+    expect(db[:sessions].order(:id).last).to_not be_nil
+  end
+
+  it 'logs exactly one sessions' do
+    expect(db[:sessions].count).to eq(1)
+  end
+end
+
+shared_examples 'set authentication context' do
+  # needs :db
+  # needs :configuration
+  # needs :frontend_container_ip
+  # needs :radius_key
+
+  let(:eapol_test_command) do
+    `eapol_test -t5 -c ./spec/support/#{configuration} -a #{frontend_container_ip} -s #{radius_key}`
+  end
+  let(:result) { eapol_test_command.split("\n").last }
+
+  before do
+    db[:userdetails].insert(
+      {
+        username: 'DSLPR',
+        contact: '+447766554430',
+        password: 'SharpRegainDetailed',
+        mobile: '+447766554430'
+      }
+    )
+  end
+end
+
+shared_examples 'a valid auth' do
+  include_examples 'set authentication context'
+
+  it 'ACCEPT' do
+    expect(result).to match('SUCCESS')
+  end
+  it_behaves_like 'a success logger'
+
+  context 'with invalid radius secret' do
+    let(:radius_key) { 'somerandomkey' }
+
+    it 'REJECT' do
+      expect(result).to eq('FAILURE')
+    end
+  end
+end
+
+shared_examples 'an invalid auth' do
+  include_examples 'set authentication context'
+
+  it 'REJECT' do
+    expect(result).to eq('FAILURE')
+  end
+
+  context 'with invalid radius secret' do
+    let(:radius_key) { 'somerandomkey' }
+
+    it 'REJECT' do
+      expect(result).to eq('FAILURE')
+    end
+  end
+end
