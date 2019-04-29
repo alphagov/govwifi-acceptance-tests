8913f1724289ba15eb0d1eee6b56abb15bff5221
Anthony King <anthony@madetech.com>
2019-04-29 10:07:39 +0100

    use recursive copy

--- ci/tasks/scripts/test.sh
+++ ci/tasks/scripts/test.sh
@@ -4,9 +4,9 @@ set -e -u -o pipefail
 
 ./src/ci/tasks/scripts/with-docker.sh
 
-cp "frontend" "src/.frontend"
-cp "authentication-api" "src/.authentication-api"
-cp "logging-api" "src/.logging-api"
+cp -r "frontend" "src/.frontend"
+cp -r "authentication-api" "src/.authentication-api"
+cp -r "logging-api" "src/.logging-api"
 
 workspace_dir="${PWD}"
 cd src
