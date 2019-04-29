274b86e43c2d64fe0da407866dc485b8a6f5670c
Anthony King <anthony@madetech.com>
2019-04-29 10:05:29 +0100

    avoid nesting inputs
    this isn't support in concourse

--- ci/tasks/scripts/test.sh
+++ ci/tasks/scripts/test.sh
@@ -4,6 +4,10 @@ set -e -u -o pipefail
 
 ./src/ci/tasks/scripts/with-docker.sh
 
+cp "frontend" "src/.frontend"
+cp "authentication-api" "src/.authentication-api"
+cp "logging-api" "src/.logging-api"
+
 workspace_dir="${PWD}"
 cd src
 
