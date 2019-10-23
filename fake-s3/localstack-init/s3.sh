c0fc6452cb59167cd3988ac04ed0c1c3ca230636
Anshul Sirur <vixus0@gmail.com>
2019-10-23 14:45:25 +0100

    Use separate whitelist and certs buckets
    This will more closely replicate the situation on AWS and what the raddb
    image expects.

--- fake-s3/localstack-init/s3.sh
+++ fake-s3/localstack-init/s3.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 echo "Init localstack s3"
-awslocal s3 mb s3://test-bucket
-awslocal s3 cp /tmp/localstack/clients.conf s3://test-bucket
-awslocal s3 sync /tmp/localstack/.certs s3://test-bucket
+awslocal s3 mb s3://whitelist-bucket
+awslocal s3 mb s3://certs-bucket
+awslocal s3 cp /tmp/localstack/clients.conf s3://whitelist-bucket
+awslocal s3 sync /tmp/localstack/.certs s3://certs-bucket
 touch /tmp/localstack/comodoCA.pem
-awslocal s3 cp /tmp/localstack/comodoCA.pem s3://test-bucket
+awslocal s3 cp /tmp/localstack/comodoCA.pem s3://certs-bucket
